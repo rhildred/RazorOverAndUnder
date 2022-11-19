7c51f45e79707de1482f2f009aa238700000b5b4
Madumetha <madudevanathan@gmail.com>
2022-11-18 23:19:39 -0500

    added unit test cases

--- OrderBot.tests/OrderBotTest.cs
+++ OrderBot.tests/OrderBotTest.cs
@@ -12,6 +12,7 @@ namespace OrderBot.tests
         {
 
         }
+
         [Fact]
         public void TestWelcome()
         {
@@ -19,13 +20,16 @@ namespace OrderBot.tests
             String sInput = oSession.OnMessage("hello")[0];
             Assert.True(sInput.Contains("Welcome"));
         }
+
         [Fact]
         public void TestShawarama()
         {
             Session oSession = new Session("12345");
             String sInput = oSession.OnMessage("hello")[0];
-            Assert.True(sInput.ToLower().Contains("shawarama"));
+            Console.WriteLine(sInput);
+            Assert.True(sInput.ToLower().Contains("sangeetha"));
         }
+
         [Fact]
         public void TestSize()
         {
@@ -33,26 +37,82 @@ namespace OrderBot.tests
             String sInput = oSession.OnMessage("hello")[1];
             Assert.True(sInput.ToLower().Contains("size"));
         }
+
         [Fact]
-        public void TestLarge()
+        public void TestMini()
         {
             Session oSession = new Session("12345");
             oSession.OnMessage("hello");
-            String sInput = oSession.OnMessage("large")[0];
-            Assert.True(sInput.ToLower().Contains("protein"));
-            Assert.True(sInput.ToLower().Contains("large"));
+            String sInput = oSession.OnMessage("mini")[0];
+            Assert.True(sInput.ToLower().Contains("mini"));
         }
+
         [Fact]
-        public void TestChicken()
+        public void TestCurry1()
         {
-            string sPath = DB.GetConnectionString();
             Session oSession = new Session("12345");
             oSession.OnMessage("hello");
-            oSession.OnMessage("large");
-            String sInput = oSession.OnMessage("chicken")[0];
-            Assert.True(sInput.ToLower().Contains("toppings"));
-            Assert.True(sInput.ToLower().Contains("large"));
-            Assert.True(sInput.ToLower().Contains("chicken"));
+            String sInput = oSession.OnMessage("mini")[0];
+            Assert.True(sInput.ToLower().Contains("mini"));
+            Assert.True(sInput.ToLower().Contains("option 1 curry"));
+        }
+
+        [Fact]
+        public void TestCurry2()
+        {
+            Session oSession = new Session("12345");
+            oSession.OnMessage("hello");
+            oSession.OnMessage("mini");
+            String sInput = oSession.OnMessage("sambar")[0];
+
+            Assert.True(sInput.ToLower().Contains("mini"));
+            Assert.True(sInput.ToLower().Contains("option 2 curry"));
+            Assert.True(sInput.ToLower().Contains("sambar"));
+        }
+
+        [Fact]
+        public void TestVeggy1()
+        {
+            Session oSession = new Session("12345");
+            oSession.OnMessage("hello");
+            oSession.OnMessage("mini");
+            oSession.OnMessage("sambar");
+            String sInput = oSession.OnMessage("rasam")[0];
+
+            Assert.True(sInput.ToLower().Contains("mini"));
+            Assert.True(sInput.ToLower().Contains("option 1 veggy"));
+            Assert.True(sInput.ToLower().Contains("rasam"));
+        }
+
+        [Fact]
+        public void TestVeggy2()
+        {
+            Session oSession = new Session("12345");
+            oSession.OnMessage("hello");
+            oSession.OnMessage("mini");
+            oSession.OnMessage("sambar");
+            oSession.OnMessage("rasam");
+            String sInput = oSession.OnMessage("carrot")[0];
+
+            Assert.True(sInput.ToLower().Contains("mini"));
+            Assert.True(sInput.ToLower().Contains("option 2 veggy"));
+            Assert.True(sInput.ToLower().Contains("carrot"));
+        }
+
+        [Fact]
+        public void TestDessert()
+        {
+            Session oSession = new Session("12345");
+            oSession.OnMessage("hello");
+            oSession.OnMessage("mini");
+            oSession.OnMessage("sambar");
+            oSession.OnMessage("rasam");
+            oSession.OnMessage("carrot");
+            String sInput = oSession.OnMessage("beans")[0];
+
+            Assert.True(sInput.ToLower().Contains("mini"));
+            Assert.True(sInput.ToLower().Contains("dessert"));
+            Assert.True(sInput.ToLower().Contains("beans"));
         }
     }
 }
